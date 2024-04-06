de51dc398db0f83d7a2edb92f7904f028ee70667
itran <itran@RISHABHPC>
2024-04-06 10:57:11 +0530

    Add project files.

---
+++ ConsoleApp1/ApplicationCode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ApplicationCode
+    {
+        //***************** Convert String to lower case
+
+        public void ConvertStringToLowercase()
+        {
+            string str = string.Empty;
+            Console.WriteLine("Enter a number:");
+            str = Console.ReadLine();
+            Console.WriteLine(str.ToLower());
+            Console.ReadLine();
+        }
+
+        //****************Reverse string******************
+        public void ReverseStringpredefineFunction()
+        {
+            string str = "Ranjan";
+            char[] strArray = str.ToCharArray();
+            Array.Reverse(strArray);
+            string rvStr = new string(strArray);
+            Console.WriteLine($"Revese str with built in function : {rvStr}");
+            Console.ReadLine();
+        }
+
+        //****************Reverse withoit predefined function******************
+        public void ReverseStringWithoutpredefineFunction()
+        {
+            string str = "Ranjan";
+            string reverse = "";
+
+            int Length = str.Length - 1;
+            var test = str[5];
+            while (Length >= 0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+            //Displaying the reverse word  
+            Console.WriteLine("Reverse word without reverse function is {0}", reverse);
+            Console.ReadLine();
+        }
+
+        //****************Reverse withoit predefined function******************
+        public void ReverseNumber()
+        {
+            Console.WriteLine("Enter a No. to reverse");
+            int Number = int.Parse(Console.ReadLine());
+            int Reverse = 0;
+            while (Number > 0)
+            {
+                int remainder = Number % 10;
+                Reverse = (Reverse * 10) + remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", Reverse);
+            Console.ReadLine();
+        }
+
+        //***********************Replace first 3 letter with #
+
+        public void ReplaceHash()
+        {
+
+            int n = 2;
+            string text = "ABCD";
+            string replacement = "##";
+            string result = replacement + text.Substring(n);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using ConsoleApp1;
+using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("New world"); 
+
+ApplicationCode appCode = new ApplicationCode();
+//appCode.ConvertStringToLowercase(); // Convert string to lowercase
+//appCode.ReverseStringpredefineFunction(); // Reverse string
+
+//appCode.ReverseStringWithoutpredefineFunction();   //***************** without reverse function
+
+//appCode.ReverseNumber();//***************** Reverse Number
+appCode.ReplaceHash();
+
+
+//************************************************
+List<string> list = new List<string>() { "Ranjan", "Rishab", "sandhya", "Amith" };
+List<int> list1 = new List<int>() { 1, 2, 3 };
+
+List<Student> std = new List<Student>() {
+  new  Student() { StudentID=1,StudentName="Ranjan",Age=10 },
+  new Student() {StudentID=2,StudentName="Rishabh",Age=20},
+  new Student() {StudentID=3,StudentName="Amith",Age=30},
+  new Student() {StudentID=4,StudentName="Sandhya",Age = 40},
+  new Student() {StudentID=4,StudentName="Nikhil",Age = 50},
+  new Student() {StudentID=4,StudentName="Rehan",Age = 60},
+  new Student() {StudentID=4,StudentName="Anuj",Age = 70}
+};
+
+var msSoD = list1.Where(x => x > 2).SingleOrDefault();
+
+var qs = (from stds in std
+          select stds.StudentID).ToList();
+var stMS = std.ToList();
+
+
+var employee = std
+   // .OrderByDescending(e => e.Age)
+   .OrderBy(x => x.Age)
+       .Skip(5)
+    .First();
+
+var ms = std.Where(std => std.Age > 30).ToList();
+
+var stu = from st in std.Where(s => s.Age > 15)
+              //select st.StudentName;
+          select new Student()
+          {
+              StudentID = st.StudentID,
+              StudentName = st.StudentName,
+              Age = st.Age
+          };
+
+var stMS1 = std.Select(x => new Student()
+{
+    StudentID = x.StudentID,
+    StudentName = x.StudentName,
+    Age = x.Age
+}
+            ).ToList();
+
+
+foreach (var st in stMS1)
+{
+    Console.WriteLine($"Id={st.StudentID},Name={st.StudentName},Age={st.Age}");
+}
+
+
+Console.ReadLine();
+
